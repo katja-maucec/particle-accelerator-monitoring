4523ee36d31bd74cdb394b9c452e94c84af507a6
Katja <katja.maucec@berkeley.edu>
2024-08-11 05:28:14 -0600

    Updated javadocs for State.java

--- State.java
+++ State.java
@@ -1,6 +1,14 @@
+/**
+ * Program begins with an OFF state.
+ * 
+ * ON: Puts the sensor on standby before it can start measuring.
+ * MEASURING: Begins data transmission.
+ * IDLE: Measuring has been paused and needs to be manually resumed.
+ * OFF: The sensor is fully shut off.
+ */
 enum State {
     ON,
-    OFF,
     MEASURING,
-    IDLE
+    IDLE,
+    OFF
 }
