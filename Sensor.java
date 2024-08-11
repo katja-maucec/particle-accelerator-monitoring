1330fd0070016d718dd1d49797df27df8684512b
Katja <katja.maucec@berkeley.edu>
2024-08-11 05:29:08 -0600

    Updated javadocs for Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -1,14 +1,12 @@
 import java.util.Random;
 
 public class Sensor {
-    //variables
     private State curState;
     private double min;
     private double max;
     private String sensorName;
     private boolean canNegative;
 
-    //constructor
     public Sensor(State initialState, double min, double max, String sensorName, boolean canNegative) {
         curState = initialState;
         this.min = min;
@@ -17,31 +15,52 @@ public class Sensor {
         this.canNegative = canNegative;
     }
 
-    //getters
+    /**
+     * 
+     * @return current state of sensor
+     */
     public State getState() {
         return this.curState;
     }
 
+    /**
+     * 
+     * @return lower bound of sensor's allowed interval
+     */
     public double getMin() {
         return this.min;
     }
 
+    /**
+     * 
+     * @return upper bound of sensor's allowed interval
+     */
     public double getMax() {
         return this.max;
     }
 
+    /**
+     * 
+     * @return name of sensor 
+     */
     public String getName() {
         return this.sensorName;
     }
 
-    //setters
+    /**
+     * 
+     * @param newState passes in what the new state of sensor will be
+     */
     public void setState(State newState) {
         this.curState = newState;
     }
     
-    /* 
-     * generate data for each sensor by randomly sampling from a gaussian
-     * distribution with a mean that is the midpoint of its allowed interval
+    /**
+     * Generate data for each sensor by randomly sampling from a gaussian
+     * distribution with a mean that is the midpoint of its allowed interval;
+     * standard deviation is scaled to the width of the interval. 
+     * 
+     * @return the pseudorandomized datapoint
      */
     public double generateData() {
         double mean = (this.min + this.max) / 2;
