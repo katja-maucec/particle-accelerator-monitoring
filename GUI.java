4c5c58b61f7940f1e6e3d7d6809c9a0cd9da928f
Katja <katja.maucec@berkeley.edu>
2024-08-11 05:01:46 -0600

    Updated color, text, and spacing formatting.

--- GUI.java
+++ GUI.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -29,13 +33,26 @@ public class GUI extends JFrame{
     //service instance variable responsible for archiving and monitoring
     Service service = new Service();
 
+    final int WIDTH = 160;
+
+    final int HEIGHT = 320;
+
+    final Font HEADER1 = new Font("Dialog", Font.BOLD, 18);
+
+    final Color COL_COLOR = new Color(206, 132, 132);
+
+    final EmptyBorder HEADER_BORDER = new EmptyBorder(20, 20, 10, 20);
+
     public GUI() {
         /* Loading GUI Frame */
         super("Particle Accelerator Monitoring");
+        setBackground(new Color(229, 191, 191));
         
         //row 1
         JLabel title = new JLabel("Live Sensor Monitoring", JLabel.CENTER);
+        title.setFont(new Font("Dialog", Font.BOLD, 30));
         title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        title.setBorder(new EmptyBorder(20, 20, 20, 20));
         add(title);
 
         //row 3
@@ -43,12 +60,26 @@ public class GUI extends JFrame{
 
         //row 3 col 1
         JPanel col1 = new JPanel();
+        col1.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        col1.setBackground(COL_COLOR);
+        col1.setLayout(new BoxLayout(col1, BoxLayout.Y_AXIS));
 
-        JLabel tempText = new JLabel("Temperature");
+        JLabel tempText = new JLabel("Temperature", JLabel.CENTER);
         JLabel curTemp = new JLabel("---");
         JLabel tempTime = new JLabel("\n");
         JLabel tempStatus = new JLabel("Status: OFF");
-        JLabel tempAlert = new JLabel("\n");
+        JLabel tempAlert = new JLabel("\n", JLabel.CENTER);
+
+        tempText.setFont(HEADER1);
+        tempText.setBorder(HEADER_BORDER);
+        tempText.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        curTemp.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        tempTime.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        tempStatus.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        tempStatus.setBorder(new EmptyBorder(0, 0, 10, 0));
+        tempAlert.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        tempAlert.setMaximumSize(new Dimension(130, 100));
+        tempAlert.setForeground(new Color(255, 255, 255));
 
         col1.add(tempText);
         col1.add(curTemp);
@@ -60,21 +91,25 @@ public class GUI extends JFrame{
         col1.add(tempOn);
         tempOn.setEnabled(true);
         tempOn.setFocusPainted(false);
+        tempOn.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         
         JButton tempMon = new JButton("Start");
         col1.add(tempMon);
         tempMon.setEnabled(false);
         tempMon.setFocusPainted(false);
+        tempMon.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         JButton tempStop = new JButton("Pause");
         col1.add(tempStop);
         tempStop.setEnabled(false);
         tempStop.setFocusPainted(false);
+        tempStop.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         
         JButton tempOff = new JButton("Turn off");
         col1.add(tempOff);
         tempOff.setEnabled(false);
         tempOff.setFocusPainted(false);
+        tempOff.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         col1.add(tempAlert);
 
@@ -120,7 +155,7 @@ public class GUI extends JFrame{
                 tempSensor.setState(State.IDLE);
 
                 //add time of last measurement
-                tempTime.setText("Taken at: " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)));
+                tempTime.setText("(Last at: " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)) + ")");
 
                 //update status
                 tempStatus.setText("Status: IDLE");
@@ -153,16 +188,28 @@ public class GUI extends JFrame{
             }
         });
 
-        col1.setLayout(new BoxLayout(col1, BoxLayout.Y_AXIS));
-
         //row 3 col 2
         JPanel col2 = new JPanel();
+        col2.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        col2.setBackground(COL_COLOR);
+        col2.setLayout(new BoxLayout(col2, BoxLayout.Y_AXIS));
 
-        JLabel humText = new JLabel("Humidity");
+        JLabel humText = new JLabel("Humidity", JLabel.CENTER);
         JLabel curHum = new JLabel("---");
         JLabel humTime = new JLabel("\n");
         JLabel humStatus = new JLabel("Status: OFF");
-        JLabel humAlert = new JLabel("\n");
+        JLabel humAlert = new JLabel("\n", JLabel.CENTER);
+
+        humText.setFont(HEADER1);
+        humText.setBorder(HEADER_BORDER);
+        humText.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        curHum.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        humTime.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        humStatus.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        humStatus.setBorder(new EmptyBorder(0, 0, 10, 0));
+        humAlert.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        humAlert.setMaximumSize(new Dimension(130, 100));
+        humAlert.setForeground(new Color(255, 255, 255));
 
         col2.add(humText);
         col2.add(curHum);
@@ -173,19 +220,26 @@ public class GUI extends JFrame{
         JButton humOn = new JButton("Turn on");
         col2.add(humOn);
         humOn.setEnabled(true);
-
+        humOn.setFocusPainted(false);
+        humOn.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         
         JButton humMon = new JButton("Start");
         col2.add(humMon);
         humMon.setEnabled(false);
+        humMon.setFocusPainted(false);
+        humMon.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         JButton humStop = new JButton("Pause");
         col2.add(humStop);
         humStop.setEnabled(false);
+        humStop.setFocusPainted(false);
+        humStop.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         
         JButton humOff = new JButton("Turn off");
         col2.add(humOff);
         humOff.setEnabled(false);
+        humOff.setFocusPainted(false);
+        humOff.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         col2.add(humAlert);
 
@@ -231,7 +285,7 @@ public class GUI extends JFrame{
                 humSensor.setState(State.IDLE);
 
                 //add time of last measurement
-                humTime.setText("Taken at: " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)));
+                humTime.setText("(Last at: " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)) + ")");
 
                 //update status
                 humStatus.setText("Status: IDLE");
@@ -264,16 +318,28 @@ public class GUI extends JFrame{
             }
         });
 
-        col2.setLayout(new BoxLayout(col2, BoxLayout.Y_AXIS));
-
         //row 3 col 3
         JPanel col3 = new JPanel();
+        col3.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        col3.setBackground(COL_COLOR);
+        col3.setLayout(new BoxLayout(col3, BoxLayout.Y_AXIS));
 
-        JLabel radText = new JLabel("Radiation");
+        JLabel radText = new JLabel("Radiation", JLabel.CENTER);
         JLabel curRad = new JLabel("---");
         JLabel radTime = new JLabel("\n");
         JLabel radStatus = new JLabel("Status: OFF");
-        JLabel radAlert = new JLabel("\n");
+        JLabel radAlert = new JLabel("\n", JLabel.CENTER);
+
+        radText.setFont(HEADER1);
+        radText.setBorder(HEADER_BORDER);
+        radText.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        curRad.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        radTime.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        radStatus.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        radStatus.setBorder(new EmptyBorder(0, 0, 10, 0));
+        radAlert.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        radAlert.setMaximumSize(new Dimension(130, 100));
+        radAlert.setForeground(new Color(255, 255, 255));
 
         col3.add(radText);
         col3.add(curRad);
@@ -284,18 +350,26 @@ public class GUI extends JFrame{
         JButton radOn = new JButton("Turn on");
         col3.add(radOn);
         radOn.setEnabled(true);
+        radOn.setFocusPainted(false);
+        radOn.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         
         JButton radMon = new JButton("Start");
         col3.add(radMon);
         radMon.setEnabled(false);
+        radMon.setFocusPainted(false);
+        radMon.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         JButton radStop = new JButton("Pause");
         col3.add(radStop);
         radStop.setEnabled(false);
+        radStop.setFocusPainted(false);
+        radStop.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         
         JButton radOff = new JButton("Turn off");
         col3.add(radOff);
         radOff.setEnabled(false);
+        radOff.setFocusPainted(false);
+        radOff.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         col3.add(radAlert);
 
@@ -341,7 +415,7 @@ public class GUI extends JFrame{
                 radSensor.setState(State.IDLE);
 
                 //add time of last measurement
-                radTime.setText("Taken at: " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)));
+                radTime.setText("(Last at: " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)) + ")");
 
                 //update status
                 radStatus.setText("Status: IDLE");
@@ -374,16 +448,28 @@ public class GUI extends JFrame{
             }
         });
 
-        col3.setLayout(new BoxLayout(col3, BoxLayout.Y_AXIS));
-
         //row 3 col 4
         JPanel col4 = new JPanel();
+        col4.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        col4.setBackground(COL_COLOR);
+        col4.setLayout(new BoxLayout(col4, BoxLayout.Y_AXIS));
 
-        JLabel presText = new JLabel("Pressure");
+        JLabel presText = new JLabel("Pressure", JLabel.CENTER);
         JLabel curPres = new JLabel("---");
         JLabel presTime = new JLabel("\n");
         JLabel presStatus = new JLabel("Status: OFF");
-        JLabel presAlert = new JLabel("\n");
+        JLabel presAlert = new JLabel("\n", JLabel.CENTER);
+
+        presText.setFont(HEADER1);
+        presText.setBorder(new EmptyBorder(20, 25, 10, 25));
+        presText.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        curPres.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        presTime.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        presStatus.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        presStatus.setBorder(new EmptyBorder(0, 0, 10, 0));
+        presAlert.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        presAlert.setMaximumSize(new Dimension(130, 100));
+        presAlert.setForeground(new Color(255, 255, 255));
 
         col4.add(presText);
         col4.add(curPres);
@@ -394,18 +480,26 @@ public class GUI extends JFrame{
         JButton presOn = new JButton("Turn on");
         col4.add(presOn);
         presOn.setEnabled(true);
+        presOn.setFocusPainted(false);
+        presOn.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         
         JButton presMon = new JButton("Start");
         col4.add(presMon);
         presMon.setEnabled(false);
+        presMon.setFocusPainted(false);
+        presMon.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         JButton presStop = new JButton("Pause");
         col4.add(presStop);
         presStop.setEnabled(false);
+        presStop.setFocusPainted(false);
+        presStop.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         
         JButton presOff = new JButton("Turn off");
         col4.add(presOff);
         presOff.setEnabled(false);
+        presOff.setFocusPainted(false);
+        presOff.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         col4.add(presAlert);
 
@@ -451,7 +545,7 @@ public class GUI extends JFrame{
                 presSensor.setState(State.IDLE);
 
                 //add time of last measurement
-                presTime.setText("Taken at: " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)));
+                presTime.setText("(Last at: " + LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)) + ")");
 
                 //update status
                 presStatus.setText("Status: IDLE");
@@ -484,8 +578,6 @@ public class GUI extends JFrame{
             }
         });
 
-        col4.setLayout(new BoxLayout(col4, BoxLayout.Y_AXIS));
-
         sensorRow.add(col1);
         sensorRow.add(col2);
         sensorRow.add(col3);
@@ -510,16 +602,16 @@ public class GUI extends JFrame{
                     //archive and monitor
                     String monitor = service.measure(tempSensor, tempVal);
                     if (monitor == "too high") {
-                        tempAlert.setText("<html>ALERT: <br/>Temperature above <br/>-268.5 \u2103</html>");
+                        tempAlert.setText("<html><center>ALERT: Temperature above -268.5 \u2103; press Start to continue measuring.</center></html>");
                         tempStop.doClick();
-                        pack();
                     } else if (monitor == "too low") {
-                        tempAlert.setText("<html>ALERT: <br/>Temperature below <br/>-269.5 \u2103</html>");
+                        tempAlert.setText("<html><center>ALERT: Temperature below -269.5 \u2103; press Start to continue measuring.</center></html>");
                         tempStop.doClick();
-                        pack();
                     } else {
                         tempAlert.setText("\n");
                     }
+
+                    pack();
                 }
 
                 if (humSensor.getState() == State.MEASURING) {
@@ -532,16 +624,13 @@ public class GUI extends JFrame{
                     //archive and monitor
                     String monitor = service.measure(humSensor, humVal);
                     if (monitor == "too high") {
-                        humAlert.setText("<html>ALERT: <br/>Humidity above <br/>10 &#37;</html>");
+                        humAlert.setText("<html><center>ALERT: Humidity above 10 &#37;; press Start to continue measuring.</center></html>");
                         humStop.doClick();
-                        pack();
-                    } else if (monitor == "too low") {
-                        humAlert.setText("<html>ALERT: <br/>Humidity below <br/>0 &#37;</html>");
-                        humStop.doClick();
-                        pack();
                     } else {
                         humAlert.setText("\n");
                     }
+
+                    pack();
                 }
 
                 if (radSensor.getState() == State.MEASURING) {
@@ -549,21 +638,18 @@ public class GUI extends JFrame{
                     double radVal = radSensor.generateData();
                 
                     //display data
-                    curRad.setText(String.format("%.3f Mev", radVal));
+                    curRad.setText(String.format("%.3f MeV", radVal));
                 
                     //archive and monitor
                     String monitor = service.measure(radSensor, radVal);
                     if (monitor == "too high") {
-                        radAlert.setText("<html>ALERT: <br/>Radiation above <br/>40 MeV</html>");
+                        radAlert.setText("<html><center>ALERT: Radiation above 40 MeV; press Start to continue measuring.</center></html>");
                         radStop.doClick();
-                        pack();
-                    } else if (monitor == "too low") {
-                        radAlert.setText("<html>ALERT: <br/>Radiation below <br/>5 MeV</html>");
-                        radStop.doClick();
-                        pack();
                     } else {
                         radAlert.setText("\n");
                     }
+
+                    pack();
                 }
 
                 if (presSensor.getState() == State.MEASURING) {
@@ -576,16 +662,13 @@ public class GUI extends JFrame{
                     //archive and monitor
                     String monitor = service.measure(presSensor, presVal);
                     if (monitor == "too high") {
-                        presAlert.setText("<html>ALERT: <br/>Pressure above <br/>1 nanombar</html>");
+                        presAlert.setText("<html><center>ALERT: Pressure above 1 nanombar; press Start to continue measuring.</center></html>");
                         presStop.doClick();
-                        pack();
-                    } else if (monitor == "too low") {
-                        presAlert.setText("<html>ALERT: <br/>Pressure below <br/>10<sup>-3</sup> nanombar</html>");
-                        presStop.doClick();
-                        pack();
                     } else {
                         presAlert.setText("\n");
                     }
+
+                    pack();
                 }
             }
         };
