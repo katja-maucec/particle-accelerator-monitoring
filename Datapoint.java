f6e025288649816bc9bc4dfa4be6f47a1a96e5f2
Katja <katja.maucec@berkeley.edu>
2024-08-11 05:37:45 -0600

    Updated javadocs for Datapoint.java

--- Datapoint.java
+++ Datapoint.java
@@ -1,5 +1,10 @@
 import java.time.LocalTime;
 
+/**
+ * Want each data measurement to be linked to
+ * a timestamp so we can display it when idle
+ * and we can save it to the archive in chronological order.
+ */
 public class Datapoint {
     double value;
     LocalTime time;
@@ -8,7 +13,4 @@ public class Datapoint {
         this.value = value;
         time = LocalTime.now();
     }
-
-    
-
 }
